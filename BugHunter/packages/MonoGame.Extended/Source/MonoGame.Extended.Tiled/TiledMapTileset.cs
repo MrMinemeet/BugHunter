31962d56e760dc6487ebf12ec20b957c42356396
MrMinemeet <31434466+MrMinemeet@users.noreply.github.com>
2018-12-03 11:40:35 +0100

    Update

---
+++ BugHunter/packages/MonoGame.Extended/Source/MonoGame.Extended.Tiled/TiledMapTileset.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MonoGame.Extended.Tiled
+{
+    public class TiledMapTileset
+    {
+        public TiledMapTileset(Texture2D texture, int tileWidth, int tileHeight, int tileCount, int spacing, int margin, int columns)
+        {
+            Texture = texture;
+            TileWidth = tileWidth;
+            TileHeight = tileHeight;
+            TileCount = tileCount;
+            Spacing = spacing;
+            Margin = margin;
+            Columns = columns;
+            Properties = new TiledMapProperties();
+            Tiles = new List<TiledMapTilesetTile>();
+        }
+
+        public string Name => Texture.Name;
+        public Texture2D Texture { get; }
+        //public int FirstGlobalIdentifier { get; internal set; }
+        public int TileWidth { get; }
+        public int TileHeight { get; }
+        public int Spacing { get; }
+        public int Margin { get; }
+        public int TileCount { get; }
+        public int Columns { get; }
+        public List<TiledMapTilesetTile> Tiles { get; }
+        public TiledMapProperties Properties { get; }
+
+        public Rectangle GetTileRegion(int localTileIdentifier)
+        {
+            return TiledMapHelper.GetTileSourceRectangle(localTileIdentifier, TileWidth, TileHeight, Columns, Margin, Spacing);
+        }
+
+        //public TiledMapTilesetAnimatedTile GetAnimatedTilesetTileByLocalTileIdentifier(int localTileIdentifier)
+        //{
+        //    throw new NotImplementedException();
+        //    //TiledMapTilesetAnimatedTile animatedTile;
+        //    //_animatedTilesByLocalTileIdentifier.TryGetValue(localTileIdentifier, out animatedTile);
+        //    //return animatedTile;
+        //}
+
+        //public bool ContainsGlobalIdentifier(int globalIdentifier)
+        //{
+        //    return globalIdentifier >= FirstGlobalIdentifier && globalIdentifier < FirstGlobalIdentifier + TileCount;
+        //}
+    }
+}
